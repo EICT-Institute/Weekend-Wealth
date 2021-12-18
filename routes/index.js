f7fd8190843efd581ead9144f7cba1148643c2f9
mbanda1 <mbandanixon@gmail.com>
2021-12-18 23:08:53 +0300

    Token and autherization

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,16 @@ const subject = require('./subject');
 const topic = require('./topic');
 const subTopic = require('./sub-topic');
 const suggestion = require('./suggestion');
+const { generateToken } = require('../utils/token');
+
+router.get('/get-token', (req, res, next) => {
+  generateToken({ _id: `${new Date()}` }).then((data) => {
+    res.status(201).json({
+      success: true,
+      data,
+    });
+  });
+});
 
 router.use('/subjects', [subject]);
 router.use('/topics', [topic]);
