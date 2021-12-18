1a64ff1307df8105bfce10e13efc96926087ff92
mbanda1 <mbandanixon@gmail.com>
2021-12-18 03:55:52 +0300

    Add new routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const subject = require('./subject');
 const topic = require('./topic');
+const subTopic = require('./sub-topic');
+const suggestion = require('./suggestion');
 
 router.use('/subjects', [subject]);
 router.use('/topics', [topic]);
+router.use('/sub-topics', [subTopic]);
+router.use('/suggestions', [suggestion]);
 
 module.exports = router;
