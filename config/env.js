0965dfdafe86c32f617ea2a642a6f54b3681452a
mbanda1 <mbandanixon@gmail.com>
2021-12-18 23:08:28 +0300

    Update enviroment varibales

--- config/env.js
+++ config/env.js
@@ -1,5 +1,10 @@
+const JWT_SECRET = process.env.JWT_SECRET;
+const NODE_ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT;
+
 module.exports = {
-  NODE_ENV: 'development',
-  PORT: 3030,
+  NODE_ENV,
+  PORT,
   LOG_PATH: 'errors/logs',
+  JWT_SECRET,
 };
